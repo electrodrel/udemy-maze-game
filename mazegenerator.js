b4a1c528f43d8555cdd101c56df098edb477667d
electrodrel <electrodrel@gmail.com>
2021-01-30 21:22:26 -0600

    Added the maze, ball and the goal

--- mazegenerator.js
+++ mazegenerator.js
@@ -1,6 +1,84 @@
 const generate = (width, height) => {
-    const cells = new Array(height);
-    for (i = 0; i < width; i++) {
-        cells[i] = new Array(width).map((value, index));
+  const grid = Array(height)
+    .fill(null)
+    .map(() => Array(width).fill(false));
+
+  const verticals = Array(height)
+    .fill(null)
+    .map(() => Array(width - 1).fill(false));
+
+  const horizontals = Array(height - 1)
+    .fill(null)
+    .map(() => Array(width).fill(false));
+
+  const startRow = Math.floor(Math.random() * height);
+  const startColumn = Math.floor(Math.random() * width);
+
+  const shuffle = (arr) => {
+    let counter = arr.length;
+
+    while (counter > 0) {
+      const index = Math.floor(Math.random() * counter);
+      counter--;
+      const temp = arr[counter];
+      arr[counter] = arr[index];
+      arr[index] = temp;
     }
-}
+
+    return arr;
+  };
+
+  const traverse = (row, column) => {
+    if (grid[row][column]) {
+      return;
+    }
+    grid[row][column] = true;
+
+    const cellNeighbors = shuffle([
+      [row - 1, column, "up"],
+      [row, column + 1, "right"],
+      [row + 1, column, "down"],
+      [row, column - 1, "left"],
+    ]);
+
+    for (let neightbor of cellNeighbors) {
+      const [nextRow, nextColumn, direction] = neightbor;
+      if (
+        nextRow < 0 ||
+        nextRow >= height ||
+        nextColumn < 0 ||
+        nextColumn >= width
+      ) {
+        continue;
+      }
+
+      if (grid[nextRow][nextColumn]) {
+        continue;
+      }
+
+      switch (direction) {
+        case "left":
+          verticals[row][column - 1] = true;
+          break;
+        case "right":
+          verticals[row][column] = true;
+          break;
+        case "up":
+          horizontals[row - 1][column] = true;
+          break;
+        case "down":
+          horizontals[row][column] = true;
+          break;
+      }
+
+      traverse(nextRow, nextColumn);
+    }
+  };
+
+  traverse(startRow, startColumn);
+
+  return {
+    verticals,
+    horizontals,
+  };
+};
